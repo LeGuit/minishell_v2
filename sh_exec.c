61dcb912bf0ad845e646022c9901ee250ce7edbb
Guillaume WOODWARD <gwoodwar@e2r12p12.42.fr>
2016-02-11 18:46:41 +0100

    path seems ok

--- sh_exec.c
+++ sh_exec.c
@@ -6,43 +6,47 @@
 /*   By: gwoodwar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/10 17:38:10 by gwoodwar          #+#    #+#             */
-/*   Updated: 2016/02/11 17:51:14 by gwoodwar         ###   ########.fr       */
+/*   Updated: 2016/02/11 18:46:26 by gwoodwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static char		*test_path(char *cursor, char *cmd, int size)
+{
+	char		*tmp;
+	struct stat	statfile;
+
+	tmp = ft_strnew(ft_strlen(cmd) + size + 1);
+	ft_strncpy(tmp, cursor, size);
+	tmp = ft_strcat(tmp, "/");
+	tmp = ft_strcat(tmp, cmd);
+	lstat(tmp, &statfile);
+	if (!statfile.st_mode & S_IXUSR)
+		ft_error_chdir(cmd);
+	else if (access(tmp, 0))
+		ft_error_chdir(cmd);
+	else
+		return (tmp);
+	return (NULL);
+}
+
 static char		*sh_fetch_in_path(char *path, char *cmd)
 {
 	int			i;
-	char		*tmp;
+	char		*res;
 	char		*tmpath;
 	char		*cursor;
-	struct stat	statfile;
 
 	tmpath = path;
 	while (tmpath)
 	{
-		ft_putstr(tmpath);
 		i = 0;
-		tmp = NULL;
 		cursor = tmpath;
-		ft_putstr("YO");
 		while (tmpath[i] != ':')
 			i++;
-		ft_putstr("YO");
-		tmp = ft_strncpy(tmp, cursor, i);
-		ft_putstr("BITCH");
-		ft_putstr(tmp);
-		tmp = ft_strcat(tmp, "/");
-		tmp = ft_strcat(tmp, cmd);
-		lstat(tmp, &statfile);
-		if (!statfile.st_mode & S_IXUSR)
-			ft_error_chdir(cmd);
-		else if (access(tmp, 0))
-			ft_error_chdir(cmd);
-		else
-			return (tmp);
+		if ((res = test_path(cursor, cmd, i)))
+			return (res);
 		tmpath = (ft_strchr(tmpath, ':') + 1);
 	}
 	ft_error_chdir(cmd);
